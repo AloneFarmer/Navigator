9a0768671372738fc9cef82929ba4d6311025eb7
AloneFarmer <zbpjob@163.com>
2022-01-09 19:43:52 +0800

    1.add:封装支持lifecycle的地图组件

---
+++ app/src/main/java/com/deepway/navigator/component/view/MapContainer.java
@@ -0,0 +1,59 @@
+package com.deepway.navigator.component.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.deepway.navigator.R;
+
+public class MapContainer extends FrameLayout implements LifecycleObserver {
+
+    private MapView mapView;
+
+    public MapContainer(Context context) {
+        this(context, null);
+    }
+
+    public MapContainer(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MapContainer(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        View root = LayoutInflater.from(getContext()).inflate(R.layout.layout_map_container, this, false);
+        mapView = root.findViewById(R.id.mapView);
+
+        MapStatus.Builder builder = new MapStatus.Builder();
+        builder.zoom(18.0f);
+        mapView.getMap().setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    private void onMapResume() {
+        mapView.onResume();
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    private void onMapPause() {
+        mapView.onPause();
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private void onMapDestroy() {
+        mapView.onDestroy();
+    }
+}
